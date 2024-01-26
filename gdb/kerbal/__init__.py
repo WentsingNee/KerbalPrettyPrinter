fa2b391ad9cc26afb1d21d6ead30cb2cedc5462b
WentsingNee <1205422966@qq.com>
2024-01-26 12:57:10 +0800

    尝试自动导入

--- gdb/kerbal/__init__.py
+++ gdb/kerbal/__init__.py
@@ -9,13 +9,35 @@
 #   all rights reserved
 #
 
-from kerbal import lookup
+# from kerbal import lookup
 
-from kerbal import any
-from kerbal import bitset
-from kerbal import container
-from kerbal import function
-from kerbal import iterator
-from kerbal import memory
-from kerbal import optional
-from kerbal import utility
+import sys
+import os
+import re
+
+ext_match = re.compile(R".*\.py$")
+
+for root, path, files in os.walk(sys.path[0]):
+    print(f"{root}   {path}   {files}")
+    # for file in files:
+    #     if not ext_match.match(file):
+    #         continue
+    #     print(f"{root}/{file}")
+    #     # __import__(file)
+
+# def traverse_dir(base, relative):
+#     path = os.path.join(base, relative)
+#     for file in os.listdir(path):
+#         file_path = os.path.join(path, file)
+#         if os.path.isdir(file_path):
+#             print("文件夹：", file_path)
+#             traverse_dir(base, file_path)
+#             continue
+#         print("文件：", file_path)
+#         print("relative：", relative)
+#         if not ext_match.match(file):
+#             continue
+#         # path
+#         __import__(file)
+#
+# traverse_dir(sys.path[0], "")
