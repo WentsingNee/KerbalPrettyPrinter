a2be96eccec15b5010c1e8563820c3f8e52724fc
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-12-27 00:08:06 +0800

    自动导入

--- gdb/kerbal/__init__.py
+++ gdb/kerbal/__init__.py
@@ -9,11 +9,16 @@
 #   all rights reserved
 #
 
-from kerbal import any
-from kerbal import bitset
-from kerbal import container
-from kerbal import function
-from kerbal import iterator
-from kerbal import memory
-from kerbal import optional
-from kerbal import utility
+import importlib
+import pathlib
+
+
+base_dir = pathlib.Path(__path__[0])
+
+for printer_file in base_dir.rglob("*Printer.py"):
+
+    relative = printer_file.relative_to(base_dir)
+    module_name = str(relative)[:-3].replace('/', '.')
+
+    importlib.import_module(f"{__name__}.{module_name}", package=__name__)
+
